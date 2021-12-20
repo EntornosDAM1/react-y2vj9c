ec72ab3b49e5d88fc583fd43c0a7a5a7226927db
EntornosDAM1 <hgomezv01@educantabria.es>
2021-12-20 13:06:44 +0100

    1 file modified

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{ useState, useRef } from 'react';
 import ComponenteListaClase from './ComponenteListaClase';
 export function Lista(props) {
-  const listaComponentes = [];
-
+  const [listaComponentes,setlistaComponentes] = useState([]);
+  const valorTextInput=useRef();
   if (props.elementos !== undefined) {
     for (let i = 0; i < props.elementos.length; i++) {
       listaComponentes.push(
@@ -14,14 +14,28 @@ export function Lista(props) {
       );
     }
   }
+  const añadir=function anadir(){
+    const newLista=listaComponentes.concat( <ComponenteListaClase
+      done={false}
+      texto={valorTextInput.current.value}
+    />
+    );
+    setlistaComponentes(newLista);
+  };
+ 
 
   return (
     <div>
       {props.titulo} - {props.icono}
       <ul>
         {listaComponentes}
-        
+        <li>
+          <input ref={valorTextInput} type="text" placeHolder="Introduce una tarea"/>
+          <br/>
+          <button  onClick={añadir}>Botón</button>
+        </li> 
       </ul>
     </div>
   );
+  
 }
