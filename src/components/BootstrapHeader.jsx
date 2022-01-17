55f00df0e733bc4dc8653020d6cc29c4809de7c0
EntornosDAM1 <hgomezv01@educantabria.es>
2022-01-17 13:33:15 +0100

    2 files modified

--- src/components/BootstrapHeader.jsx
+++ src/components/BootstrapHeader.jsx
@@ -10,15 +10,16 @@ class BootStrapHeader extends React.Component {
   }
 
   render(){
-    return <Navbar variant="dark" bg="dark" expand="lg">
+    return <Navbar variant="dark" bg="white" expand="lg">
     <Container fluid>
-    <img
+ 
+          <img
             className="logo-image"
             src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
           />
-          <i className="fas fa-user-graduate" />
+          <i className="fas fa-user-graduate"/>
 
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand as={Link} to="#home">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-dark-example" />
       <Navbar.Collapse id="navbar-dark-example" className="justify-content-center" >
         <Nav>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ class Header extends React.Component {
             className="logo-image"
             src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
           />
-          <i className="fas fa-user-graduate" />
+          <i className="fas fa-user-graduate"/>
         </div>
         <div className="menu-icon" onClick={this.changeClicked.bind(this)}>
           <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} />
