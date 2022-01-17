b353b1e8b922acb904b33a1231f59eb4836c2765
EntornosDAM1 <hgomezv01@educantabria.es>
2022-01-17 13:13:18 +0100

    1 file modified

--- src/components/BootstrapHeader.jsx
+++ src/components/BootstrapHeader.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Navbar,Container,Nav,NavDropdown} from  'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {MenuItems} from '../data/MenuItems';
+
+
 class BootStrapHeader extends React.Component {
   constructor(props){
     super(props);
@@ -13,18 +17,17 @@ class BootStrapHeader extends React.Component {
             src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
           />
           <i className="fas fa-user-graduate" />
+
       <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-dark-example" />
-      <Navbar.Collapse id="navbar-dark-example">
-        <Nav>
-          <NavDropdown
-            id="nav-dropdown-dark-example"
-            title="Menu"
-            menuVariant="dark"
-          >
-            <NavDropdown.Item href="#action/3.2">Home</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Lista de Tareas</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.4">Perfil</NavDropdown.Item>
+      <Navbar.Collapse id="navbar-dark-example" >
+        <Nav className="justify-content-end">
+          <NavDropdown id="nav-dropdown-dark-example" title="Menu" menuVariant="dark">
+            {MenuItems.map((item)=>{
+                  return <NavDropdown.Item as={Link} to={item.path}>{item.title}</NavDropdown.Item>
+                }
+              )
+            }
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
