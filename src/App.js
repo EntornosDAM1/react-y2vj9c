96e1724ac3407361293f2f1be6b71a0a68fbe586
EntornosDAM1 <hgomezv01@educantabria.es>
2022-01-17 11:40:01 +0100

    8 files modified, 6 added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Lista from './components/Lista';
-import ListaClase from './components/ListaClase';
-import ComponenteHeader from './components/ComponenteHeader';
+import { Home } from './components/Home';
+import { PaginaListas } from './components/PaginaListas';
+import { Perfil } from './components/Perfil';
+import Header from './components/Header';
+import { MenuItems } from './data/MenuItems';
 
 export function App() {
-  const elementos1 = [
-    { done: false, texto: 'Hacer la compra', prioridad: 'alta' },
-    { done: true, texto: 'Sacar al perro', prioridad: 'media' },
-  ];
-  const elementos2 = [
-    { done: false, texto: 'Tomates' },
-    { done: true, texto: 'Queso' },
-    { done: false, texto: 'Leche' },
-  ];
-  const elementos3 = [{ done: false, texto: 'Estudiar React.js' }];
-
   return (
-    <div>
-      <ComponenteHeader titulo="HEADER"/>
-      <h1>Listas de tareas</h1>
-      <Lista
-        titulo="Tareas de la casa (funciones)"
-        icono="♥"
-        elementos={elementos1}
-      />
-      <ListaClase
-        titulo="ToDos del curso (clases)"
-        icono="🙌"
-        elementos={elementos3}
-      />
-    </div>
+    <Router>
+      <Header />
+      {MenuItems.map((item) => {
+        return (
+          <Route
+            key={item.id}
+            path={item.path}
+            exact
+            component={item.component}
+          />
+        );
+      })}
+    </Router>
   );
 }
